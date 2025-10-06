6c9f9df384da8b834abd837959b03c49948b5681
KhanSamiulArefin <iamsagoto@gmail.com>
2025-10-06 22:26:17 +0600

    Registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import './assets/css/style.css'
+import Main from './components/Main'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Register from './components/Register'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Main from './components/Main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
-
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </>
 
   )
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Button from './Button'
-import Header from './Header'
 import Footer from './Footer'
 
 const Main = () => {
   return (
     <>
-   
+  
     <div className='container'>
         <div className='p-5 text-center bg-light-dark rounded'>
             <h1 className='text-light'>This is the stock prediction portal</h1>
@@ -14,6 +13,7 @@ const Main = () => {
             <Button text='Login' class='btn-outline-info'/>
         </div>
     </div>
+    <Footer/>
    
     </>
     
