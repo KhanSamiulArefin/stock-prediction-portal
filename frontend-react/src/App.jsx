da2bfa2070a39cc6e980cc2e1d70386c49915898
KhanSamiulArefin <iamsagoto@gmail.com>
2025-10-11 11:39:46 +0600

    login functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -5,20 +5,26 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Register from './components/Register'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Login from './components/Login'
+import AuthProvider from './AuthProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
     <Header/>
     <Routes>
       <Route path='/' element={<Main/>}/>
       <Route path='/register' element={<Register/>}/>
+      <Route path ="/login" element={<Login/>}/>
     </Routes>
     <Footer/>
     </BrowserRouter>
+    </AuthProvider>
+    
     </>
 
   )
